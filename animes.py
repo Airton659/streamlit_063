770e753992da3614b6a308697ca0060dbf65f8c7
JoseH <jose.airton.junior@gmail.com>
2024-05-24 15:12:57 -0300

    Detalhes e comentários

--- animes.py
+++ animes.py
@@ -29,6 +29,17 @@ st.set_page_config(
     initial_sidebar_state='expanded'
 )
 
+# st.markdown(
+#     """
+#     <style>
+#     .stApp {
+#         background-color: white;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
+
 # Título da página
 st.title('Top 1000 Animes 2024')
 
@@ -37,24 +48,44 @@ st.title('Top 1000 Animes 2024')
 st.sidebar.markdown("José Airton Marques Júnior - 063", unsafe_allow_html=True)
 
 # Filtro por Gênero
-generos = df['MainGenres'].unique().tolist()
-genero_selecionado = st.sidebar.multiselect('Selecione o Gênero:', generos)
+generos = df['MainGenres'].unique().tolist() #Criando uma lista com as opções disponíveis na coluna MainGenres
+genero_selecionado = st.sidebar.multiselect('Selecione o Gênero:', generos) #genero_selecionado recebe as multiplas opções selecionadas no filtro
 
 # Filtro por Tipo
 tipos = df['Type'].unique().tolist()
 tipo_selecionado = st.sidebar.multiselect('Selecione o Tipo:', tipos)
 
 # Verificar se nenhum tipo foi selecionado
-if not tipo_selecionado:
+if not tipo_selecionado: #verifica se not tipo_selecionado é False (preenchida) ou True (Sem seleção), caso seja true, define como todos os tipos
     tipo_selecionado = tipos
 
-# Filtro por Popularidade
-popularidade_min = st.sidebar.slider('Popularidade Mínima:', min_value=df['Popularity'].min(), max_value=df['Popularity'].max(), value=df['Popularity'].min())
-popularidade_max = st.sidebar.slider('Popularidade Máxima:', min_value=df['Popularity'].min(), max_value=df['Popularity'].max(), value=df['Popularity'].max())
+# # Filtro por Popularidade
+# popularidade_min = st.sidebar.slider('Popularidade Mínima:', min_value=df['Popularity'].min(), max_value=df['Popularity'].max(), value=df['Popularity'].min())
+# popularidade_max = st.sidebar.slider('Popularidade Máxima:', min_value=df['Popularity'].min(), max_value=df['Popularity'].max(), value=df['Popularity'].max())
 
-# Filtro por Rank
-rank_min = st.sidebar.slider('Rank Mínimo:', min_value=int(df['Rank'].min()), max_value=int(df['Rank'].max()), value=int(df['Rank'].min()))
-rank_max = st.sidebar.slider('Rank Máximo:', min_value=int(df['Rank'].min()), max_value=int(df['Rank'].max()), value=int(df['Rank'].max()))
+# Filtro por Popularidade (Range Slider)
+popularidade_range = st.sidebar.slider(
+    'Selecione o intervalo de Popularidade:',
+    min_value=int(df['Popularity'].min()),
+    max_value=int(df['Popularity'].max()),
+    value=(int(df['Popularity'].min()), int(df['Popularity'].max()))
+)
+
+popularidade_min, popularidade_max = popularidade_range
+
+
+rank_range = st.sidebar.slider(
+    'Selecione o intervalo de Popularidade:',
+    min_value=int(df['Rank'].min()),
+    max_value=int(df['Rank'].max()),
+    value=(int(df['Rank'].min()), int(df['Rank'].max()))
+)
+
+rank_min, rank_max = rank_range
+
+# # Filtro por Rank
+# rank_min = st.sidebar.slider('Rank Mínimo:', min_value=int(df['Rank'].min()), max_value=int(df['Rank'].max()), value=int(df['Rank'].min()))
+# rank_max = st.sidebar.slider('Rank Máximo:', min_value=int(df['Rank'].min()), max_value=int(df['Rank'].max()), value=int(df['Rank'].max()))
 
 
 
@@ -77,15 +108,17 @@ col1, col2 = st.columns(2)
 
 # Gráfico de barras para gêneros
 with col1:
-    st.subheader("Número de Animes por Gênero")
+    st.subheader("Número de Animes por Gênero (Filtrado)")
     soma_por_genero = df_filtrado['MainGenres'].value_counts().reset_index()
     soma_por_genero.columns = ['MainGenres', 'Count']
     fig1 = px.bar(soma_por_genero, x='Count', y='MainGenres', orientation='h', 
                  labels={'Count': 'Número de Animes', 'MainGenres': 'Gênero'},
-                 color_discrete_sequence=[cor_padrao])
+                 color_discrete_sequence=[cor_padrao],
+                 width=400, height=300)
     fig1.update_layout(legend=dict(font=dict(size=8)))
     st.plotly_chart(fig1, use_container_width=True)
     st.write("Este gráfico mostra o número de animes por gênero.")
+
 
 # Gráfico de pizza para status dos animes
 with col2:
@@ -97,9 +130,9 @@ with col2:
     
     status_counts = df_filtrado['Status'].value_counts()
     fig2 = px.pie(values=status_counts, names=status_counts.index, 
-                  color_discrete_sequence=[cor_padrao])
+                  color_discrete_sequence=[cor_padrao],
+                  width=400, height=300)
     
-    # Alterar a legenda
     fig2.update_traces(textposition='inside', textinfo='percent',insidetextfont=dict(color='white'))
     
     st.plotly_chart(fig2, use_container_width=True)
@@ -110,7 +143,8 @@ st.subheader("Score por Gênero")
 df_filtrado['Outlier_Info'] = df_filtrado.apply(lambda row: f"Rank: {row['Rank']}, English: {row['English']}" if row['Score'] > df_filtrado['Score'].quantile(0.95) else '', axis=1)
 fig5 = px.box(df_filtrado, x='MainGenres', y='Score',
              labels={'MainGenres': 'Gênero', 'Score': 'Pontuação'},
-             color_discrete_sequence=[cor_padrao])
+             color_discrete_sequence=[cor_padrao],
+             width=400, height=300)
 
 fig5.update_traces(marker=dict(color='rgb(158,202,225)', size=7),
                   selector=dict(type='box'), boxpoints='outliers', hoverinfo='y+text',
@@ -126,14 +160,16 @@ with col1:
     fig3 = px.scatter(df_filtrado, x='Popularity', y='Score',
                       hover_name=df_filtrado.apply(lambda row: row['English'] if pd.notnull(row['English']) else row['Japanese'], axis=1),
                       hover_data=['Rank', 'MainGenres'],
-                      color_discrete_sequence=[cor_padrao])
+                      color_discrete_sequence=[cor_padrao],
+                      width=400, height=300)
     st.plotly_chart(fig3, use_container_width=True)
     st.write("Este gráfico mostra a relação entre popularidade e score dos animes.")
 
 # Histograma para Duração dos Episódios
 with col2:
     st.subheader("Histograma da Duração dos Animes")
-    fig4 = px.histogram(df_filtrado, x='Duration', color_discrete_sequence=[cor_padrao])
+    fig4 = px.histogram(df_filtrado, x='Duration', color_discrete_sequence=[cor_padrao],
+                 width=400, height=300)
     st.plotly_chart(fig4, use_container_width=True)
     st.write("Este histograma mostra a distribuição da duração dos episódios dos animes.")
 
